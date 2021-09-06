2ee5bcd87c46c49bd7e34895b2b29d4267132aa0
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-06 14:28:13 +0530

    Update about.py

--- AnkiVector/modules/about.py
+++ AnkiVector/modules/about.py
@@ -1,14 +1,6 @@
 __help__ = """
-** Anki Vector - A Powerful Telegram Group Manager 🎶 **
+** ATHENA - A Powerful Telegram Group Manager 🎶 **
 
-Powerful Abilities
 
-• Group Voice Chat Music Play ❤️
-
-• File To Link and URL Upload 📂
-
-• Youtube Downloader 🎵
-
-Developer [Damantha🇱🇰](https://t.me/Damantha_Jasinghe)
 """
-__mod_name__ = "About"
+__mod_name__ = "ATHENA"
