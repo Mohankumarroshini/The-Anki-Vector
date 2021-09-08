0f9bb88276c2c9468efec16aef7579e64730d25a
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-08 16:44:09 +0530

    Update logomaker.py

--- AnkiVector/modules/logomaker.py
+++ AnkiVector/modules/logomaker.py
@@ -175,7 +175,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "AnkiVectorLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @Athena_support)
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Athena_support")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
