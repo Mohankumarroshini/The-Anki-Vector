c3f00b448f41039ffed939893b7f39346d49ac97
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-09-08 06:37:27 +0530

    Update about.py

--- AnkiVector/modules/about.py
+++ AnkiVector/modules/about.py
@@ -1,6 +1,26 @@
 __help__ = """
-** ATHENA - A Powerful Telegram Group Manager 🎶 **
+** ℹ️ I'm Athena, a powerful group management bot built to help you manage your group easily.
 
+                 ❍ I can restrict users.
+
+                 ❍ I can greet users with customizable welcome messages and even set a group's rules.
+
+                 ❍ I have an advanced anti-flood system.
+
+                 ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
+
+                 ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
+
+                 ❍ I check for admins' permissions before executing any command and more stuffs
+
+Athena's licensed under the GNU General Public License v3.0
+
+Here is the [Athena Support.](text://t.me/Athena_support)
+
+If you have any question about Athena, let us know at @Athena_Support. **
 
 """
+
+
+
 __mod_name__ = "ATHENA"
